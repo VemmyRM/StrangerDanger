bce59d1853c7f56731a5d5087cfb134860722e44
VemmyRM <kwvembu@gmail.com>
2021-04-24 17:08:26 -0400

    getting users current location

--- venushacks/src/Components/GoogleMaps.js
+++ venushacks/src/Components/GoogleMaps.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   GoogleMap,
   LoadScript,
@@ -10,15 +10,26 @@ const containerStyle = {
   height: "400px",
 };
 
-const center = {
-  lat: -3.745,
-  lng: -38.523,
-};
-
 function MyComponent() {
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+    });
+  });
+
+  const [lat, setLat] = useState(0);
+  const [long, setLong] = useState(0);
+
   return (
     <LoadScript googleMapsApiKey="AIzaSyB5GC3ltAHoN99BD0nUPOSJ9T3HTav9e6U">
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={{ lat: lat, lng: long }}
+        zoom={10}
+      >
         {/* Child components, such as markers, info windows, etc. */}
         <></>
       </GoogleMap>
