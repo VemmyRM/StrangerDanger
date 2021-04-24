571121eafe3c6d37976a913ea0d6cb5c4e2e91ad
Prithika Hariharan <prithika.hariharan@gmail.com>
2021-04-24 18:52:49 -0400

    Setting up database

--- venushacks/src/Components/Form.js
+++ venushacks/src/Components/Form.js
@@ -1,43 +1,88 @@
 import Map from "./GoogleMaps";
+import firebase from "firebase";
+import React, { useState } from "react";
 
 const Form = (props) => {
+
+  var database = firebase.database();
+
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState(0);
+  const [friendName, setFriendName] = useState("");
+  const [friendNumber, setFriendNumber] = useState(0);
+  //const [friends, setFriends] = useState([]);
+  var fireRef = database.ref('users/' + name + '/friends');
+  var newUserRef = fireRef.push();
+
+  function sendData() {
+    console.log("name: "+name);
+    console.log("number: "+ number);
+    database.ref('users/' + name).set({
+      username: name,
+      number: number
+      });
+
+    // database.ref('users/' + name + '/friends').set({
+    //     Name: friendName,
+    //     number: friendNumber
+    //     });
+      
+    newUserRef.set({ 'name': friendName, 'number': friendNumber });
+
+  }
+  
   return (
     <div>
       <div className="mb-3">
         <label for="exampleInputEmail1" className="form-label">
-          Email address
+          Name
         </label>
         <input
-          type="email"
+          type=""
           className="form-control"
           id="exampleInputEmail1"
           aria-describedby="emailHelp"
+          onChange={event => setName(event.target.value)}
         />
-        <div id="emailHelp" className="form-text">
-          We'll never share your email with anyone else.
-        </div>
       </div>
       <div className="mb-3">
-        <label for="exampleInputPassword1" className="form-label">
-          Password
+        <label for="exampleInputEmail1" className="form-label">
+          Number
         </label>
         <input
-          type="password"
+          type=""
           className="form-control"
-          id="exampleInputPassword1"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          onChange={event => setNumber(event.target.value)}
         />
       </div>
-      <div className="mb-3 form-check">
-        <input
-          type="checkbox"
-          className="form-check-input"
-          id="exampleCheck1"
-        />
-        <label className="form-check-label" for="exampleCheck1">
-          Check me out
+      <div className="mb-3">
+        Enter your friends names!
+        <label for="exampleInputEmail1" className="form-label">
+          Name
         </label>
+        <input
+          type=""
+          className="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          onChange={event => setFriendName(event.target.value)}
+        />
       </div>
-      <button type="submit" className="btn btn-primary">
+      <div className="mb-3">
+        <label for="exampleInputEmail1" className="form-label">
+          Number
+        </label>
+        <input
+          type=""
+          className="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          onChange={event => setFriendNumber(event.target.value)}
+        />
+      </div>
+      <button type="submit" className="btn btn-primary" onClick={sendData}>
         Submit
       </button>
     </div>
--- venushacks/src/index.js
+++ venushacks/src/index.js
@@ -1,8 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import App from "./App";
 import Form from "./Components/Form";
 import reportWebVitals from "./reportWebVitals";
+import firebase from "firebase/app";
+
+var firebaseConfig = {
+
+    // apiKey: process.env.REACT_APP_API_KEY,
+  // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  // databaseURL: process.env.REACT_APP_DATABASE_URL,
+  // projectId: process.env.REACT_APP_PROJECT_ID,
+  // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  // appId: process.env.REACT_APP_ID,
+  // measurementId: process.env.REACT_APP_MEASUREMENT_ID
+
+  apiKey: "AIzaSyCDQpNTi8faf3lkaXkgVNn9K_eM3iP3oVQ",
+  authDomain: "venushacks-2a4dc.firebaseapp.com",
+  projectId: "venushacks-2a4dc",
+  storageBucket: "venushacks-2a4dc.appspot.com",
+  messagingSenderId: "413195790958",
+  appId: "1:413195790958:web:284fb251f84b853f15547f",
+  measurementId: "G-EEBL46T565"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +35,5 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
