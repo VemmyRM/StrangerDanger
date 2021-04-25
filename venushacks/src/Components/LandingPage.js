bcc996aae19fbd2dd54c4904db6fd483edc7e160
simrandhillon01 <45011830+simrandhillon01@users.noreply.github.com>
2021-04-25 02:17:48 -0400

    Update LandingPage.js

--- venushacks/src/Components/LandingPage.js
+++ venushacks/src/Components/LandingPage.js
@@ -10,8 +10,9 @@ function LandingPage() {
 
   return (
     <div>
+      style={{ marginRight: 10 }}
       <Navbar bg="dark" variant="dark">
-        <Navbar.Brand>Stranger Danger</Navbar.Brand>
+        <Navbar.Brand className = {{ marginLeft: 5 }} >Stranger Danger</Navbar.Brand>
           <Nav className="mr-auto">
             <Nav.Link type="submit" href = '/signup'>Login</Nav.Link>
             <Nav.Link type="submit" href = '/submitinfo'>Guest</Nav.Link>
